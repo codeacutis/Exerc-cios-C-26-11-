d9a5e060f7adf510090d2374572d3c2aada9473f
codeacutis <168932847+codeacutis@users.noreply.github.com>
2024-11-26 15:19:26 -0300

    Add files via upload

---
+++ exerc1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <strings.h>
+
+int main(){
+    char palavra[61], palavra1[61], palavra2[61];
+
+    /*printf("Digite uma palavra: ");
+    scanf ("%s", palavra);
+
+    printf("\nA palavra que voce digitou eh: %s", palavra);*/
+
+    printf ("Digite uma palavra: ");
+    scanf ("%s", palavra1);
+    printf ("Digite outra palavra: ");
+    scanf ("%s", palavra2);
+
+    printf ("\nAs palavras digitadas sao:\n%s\n%s", palavra1, palavra2);
+}
